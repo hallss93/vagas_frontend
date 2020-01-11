e2d3808eb4f1896383f51b57cc2394052bde2d02
Hallison Melo <hallss93@hotmail.com>
2020-01-11 12:51:23 -0300

    App Info

--- components/app-info.vue
+++ components/app-info.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="row">
-    <div class="col-6">
-      <h1>Título da página</h1>
-      <h3>
+    <div class="col-6 app-info">
+      <h1 class="title">Título da página</h1>
+      <h3 class="description">
         Informação curta e direta sobre o app divulgado na página com foco nas
         funcionalidades.
       </h3>
+      <div class="buttons-container">
+        <img src="@/assets/imgs/google-play.png" alt="Play Store" />
+        <img
+          src="@/assets/imgs/download-on-the-app-store-badge-ptbr-rgb-blk-092917.png"
+          alt="Play Store"
+        />
+      </div>
     </div>
 
     <div class="col-6">
