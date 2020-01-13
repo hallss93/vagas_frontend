83fd6e5fbc2a724b5263945fe11a392ccb78fc00
Hallison Melo <hallss93@hotmail.com>
2020-01-13 14:46:31 -0300

    Video

--- components/video.vue
+++ components/video.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
+  <div class="video-container">
+    <h1 class="title">Vídeo promocional do app</h1>
+    <h3 class="description">
+      O vídeo mostra todas as funcionalidades e você vai conferir como funciona
+      e ver como é fácil e simples de usar o app, falando do uso e seus
+      benefícios.
+    </h3>
     <iframe
       width="100%"
-      height="100%"
+      height="500px"
       src="https://www.youtube.com/embed/tgbNymZ7vqY"
     >
     </iframe>
