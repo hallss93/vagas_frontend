75dd47c41129a6160ce08bc199f573e8b92776ec
Hallison Melo <hallss93@hotmail.com>
2020-01-13 22:14:24 -0300

    Products

--- components/products.vue
+++ components/products.vue
@@ -1,27 +1,49 @@
 <template>
-  <section class="container">
-    <h1 class="title medium">Produtos mais acessados.</h1>
-
-    <div v-swiper:mySwiper="swiperOption">
-      <div class="swiper-wrapper">
-        <div
-          v-for="(p, index) in productsAll"
-          :key="index"
-          class="swiper-slide"
-        >
-          {{ index }}
+  <section class="product-container">
+    <div class="container mb-10">
+      <h1 class="title medium ml-40">Produtos mais acessados.</h1>
+    </div>
+    <div class="row justify-space-between">
+      <div @click="mySwiper.slidePrev()" class="swiper-button prev">
+        <img src="@/assets/imgs/ic-navigate-next.png" class="prev" alt="" />
+      </div>
+      <div class="swiper-body">
+        <div v-swiper:mySwiper="swiperOption">
+          <div class="swiper-wrapper">
+            <div
+              v-for="(p, index) in productsAll"
+              :key="index"
+              class="swiper-slide"
+            >
+              <div class="card product">
+                <span
+                  :class="{ down: p.down, up: p.up, stay: p.stay }"
+                  class="percentage"
+                  ><img v-if="p.down" src="@/assets/imgs/triangle-copy.png" />
+                  <img
+                    v-else-if="p.up"
+                    src="@/assets/imgs/triangle-copy_2.png"
+                  />
+                  <img
+                    v-else-if="p.stay"
+                    src="@/assets/imgs/triangle-copy.png"
+                  />
+                  $ {{ p.percentage }}</span
+                >
+                <img :src="p.image" :alt="p.image" class="img-product" />
+                <h4 class="product-name">{{ p.title }}</h4>
+                <h5 class="product-store">
+                  Vendido por <span class="store-name">{{ p.store.name }}</span>
+                </h5>
+                <h2 class="product-price">{{ p.price }}</h2>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div
-        slot="button-prev"
-        @click="mySwiper.slidePrev()"
-        class="swiper-button-prev"
-      ></div>
-      <div
-        slot="button-next"
-        @click="mySwiper.slideNext()"
-        class="swiper-button-next"
-      ></div>
+      <div @click="mySwiper.slideNext()" class="swiper-button next">
+        <img src="@/assets/imgs/ic-navigate-next.png" class="next" alt="" />
+      </div>
     </div>
   </section>
 </template>
@@ -50,15 +72,11 @@ export default {
       storesAll: [],
       swiperOption: {
         slidesPerView: 4,
-        spaceBetween: 30,
+        spaceBetween: 0,
         slidesPerGroup: 1,
         loop: true,
         loopFillGroupWithBlank: true,
-        pagination: false,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev'
-        }
+        pagination: false
       }
     }
   },
@@ -66,13 +84,43 @@ export default {
     this.fetchStores()
   },
   methods: {
+    _reduceString(str) {
+      return str.length > 50 ? `${str.substring(0, 50)}...` : str
+    },
+    _fixed(num, float) {
+      return parseFloat(num).toFixed(float)
+    },
+    _money(num) {
+      return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(num)
+    },
+    _calcPercentage(value, percentage) {
+      const minus = value * (percentage / 100)
+      return minus
+    },
     async fetchProducts() {
       const { data, status } = await products.getProducts()
       if (status === 200) {
-        const newArray = data.map(
-          (p) => (p.store = this.storesAll.find((el) => p.storeid === el.id))
-        )
-        this.productsAll = newArray
+        data.map((p) => {
+          const title = this._reduceString(p.title)
+          const percentage = this._fixed(
+            this._calcPercentage(p.price, p.percentage),
+            0
+          )
+          this.productsAll.push({
+            ...p,
+            title,
+            price: this._money(this._fixed(p.price, 2)),
+            percentage,
+            pp: p.percentage,
+            up: p.percentage > 0,
+            down: p.percentage < 0,
+            stay: p.percentage === 0,
+            store: this.storesAll.find((el) => p.storeid === el.id)
+          })
+        })
       }
     },
     async fetchStores() {
